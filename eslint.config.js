269d2073919108425eeda8cf95142fe4223905b8
t-ortuzar <tomas.ortuzar98@gmail.com>
2026-05-14 16:55:23 -0300

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist'],
+  },
 
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    ...js.configs.recommended,
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    ...reactHooks.configs.flat.recommended,
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    ...reactRefresh.configs.vite,
-  },
+  js.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
